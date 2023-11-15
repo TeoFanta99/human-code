77e317178647f6e9bcd3fb039c6b691cf801770c
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-11-15 17:21:47 +0100

    upload files

---
+++ ex.js
@@ -0,0 +1,29 @@
+// LASAGNE ALLA BOLOGNESE
+
+// START
+
+// Verifica ingredienti e utensili
+    // Ci sono gli ingredienti? (se SI, vai a riga 9; se NO, vai a comprarli e poi a riga 9)
+    // Ci sono gli utensili? (se SI, vai a riga 9; se NO, vai a comprarli e poi a riga 9)
+
+// Prepara il sugo alla bolognese
+    // Cuoci la carne macinata
+    // Aggiugni sedano e carota tritati
+    // Incorpora i pomodori pelati e cuoci lentamente
+
+// Prepara la besciamella
+    // Sciogli il burro in una pentola
+    // Aggiungi farina e latte
+
+// Assemblaggio
+    // Uno strato di besciamella sul fondo della teglia
+    // Pasta per lasagne
+    // Sugo bolognese
+    // Ripeti il processo per più strati
+    // Concludi con uno strato di besciamella
+
+// Cuoci in forno
+    // Riscalda il forno
+    // Cuoci fino a doratura
+
+// FINE 
